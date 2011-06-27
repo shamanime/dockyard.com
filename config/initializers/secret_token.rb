f2af1fc75c11e631dde8b8459ff1983846cd6542
Brian Cardarella <bcardarella@gmail.com>
2011-06-27 16:10:56 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dockyard::Application.config.secret_token = 'e0e42ca6895ec91664036f274f02984cde4ff1e0bb860685691d854b482fd3119f6ba54c8f42c2c47b938ee36c862fe9f234d5a8e7b1d5afa71a45173f6c2553'
