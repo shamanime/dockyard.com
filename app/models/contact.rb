ecbaf5f126bd0a8ffaf6ba92a9c75876c33b462c
Brian Cardarella <bcardarella@gmail.com>
2011-07-11 11:16:37 -0400

    right single quote

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,7 @@ class Contact
 
   attr_accessor :company_name, :contact_name, :contact_email, :message
 
-  validates_presence_of :company_name, :contact_name, :contact_email, :message
+  validates_presence_of :company_name, :contact_name, :contact_email, :message, :message => 'can&#146;t be blank'
   validates :contact_email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'invalid email format' }
 
   def persisted?
