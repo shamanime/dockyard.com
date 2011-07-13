96229541c34a14885ea0d10aeffc56da121e25ea
Brian Cardarella <bcardarella@gmail.com>
2011-07-13 16:43:22 -0400

    UTF-8 character

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,12 @@
+# encoding: UTF-8
+
 class Contact
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
   attr_accessor :company_name, :contact_name, :contact_email, :message
 
-  validates_presence_of :company_name, :contact_name, :contact_email, :message, :message => 'can&#146;t be blank'
+  validates_presence_of :company_name, :contact_name, :contact_email, :message, :message => %{can’t be blank}
   validates :contact_email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => 'invalid email format' }
 
   def initialize(attributes = {})
