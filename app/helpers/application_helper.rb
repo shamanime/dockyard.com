3b08ddb1f72e5f17cc0cf02a96df65c57d6cc5b7
Brian Cardarella <bcardarella@gmail.com>
2011-07-14 03:03:14 -0400

    Only render the now flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
 
   def render_flash
     if flash[:success]
-      content_tag(:div, :class => [:message, :success]) { flash[:success] }
+      content_tag(:div, :class => [:message, :success]) { flash.now[:success] }
     elsif flash[:error]
-      content_tag(:div, :class => [:message, :error])   { flash[:error]   }
+      content_tag(:div, :class => [:message, :error])   { flash.now[:error]   }
     end
   end
 end
