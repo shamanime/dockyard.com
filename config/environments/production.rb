788c26566d3e502556798baaafe451e4ec6a0aef
Brian Cardarella <bcardarella@gmail.com>
2011-10-10 21:20:08 -0400

    Added Sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,4 +54,15 @@ Dockyard::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'dockyard.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
