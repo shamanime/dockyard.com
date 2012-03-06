62ff5918e674e1f19cb9acabb96570e8d949fd31
Brian Cardarella <bcardarella@gmail.com>
2012-03-06 16:36:12 -0500

    precompile contact enquiry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Dockyard::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( contact_enquiry.js contact_enquiry.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
