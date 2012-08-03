610d657baf7906e1f61223943e368deb1f7eb1f0
Brian Cardarella <bcardarella@gmail.com>
2012-08-03 11:40:51 -0400

    Generated clean Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dockyard::Application.config.secret_token = 'eb463ecb1e5f768aa6fb6bf721ef23f12f727aaf0c5bb141d30c88e4e5d78f2f38e618a96050dbd81098314eb8370f0feae3d8c155f0e5c854ec7b34796ef41b'
