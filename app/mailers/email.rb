8f1d1cd86db386433b70de1b8f114168119c975e
Brian Cardarella <bcardarella@gmail.com>
2012-08-03 11:36:07 -0400

    Removed old app

--- app/mailers/email.rb
+++
@@ -1,7 +0,0 @@
-class Email < ActionMailer::Base
-  default :to => 'contact@dockyard.com'
-
-  def contact(attributes = {})
-    mail :from => attributes[:from], :subject => 'New Contact Enquiry', :body => attributes[:body]
-  end
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Dockyard::Application.routes.draw do
-  get  '/contact_enquiry' => 'contacts#new', :as => :new_contact_enquiry
-  post '/' => 'contacts#create', :as => :contacts
-  root :to => 'home#index'
-end
