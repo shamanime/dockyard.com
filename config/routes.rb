a174b10f4ddd6d51eceac1231ebd33c9f7f5bd3e
acacheung <acacheung@gmail.com>
2012-08-17 09:57:36 -0400

    added blog to nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Dockyard::Application.routes.draw do
   %w{work process people community contact}.each do |page|
     get "/#{page}" => "pages##{page}", :as => page
   end
+  %w{openbay tip repurpose}.each do |page|
+    get "/work/#{page}" => "pages##{page}", :as => page
+  end
   root :to => 'pages#landing'
 end
