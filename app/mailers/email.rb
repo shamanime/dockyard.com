415e9965af7e890bbeeb64c6ae76d72eb8994df7
Brian Cardarella <bcardarella@gmail.com>
2012-12-03 14:18:44 -0500

    Sales

--- app/mailers/email.rb
+++ app/mailers/email.rb
@@ -1,6 +1,6 @@
 class Email < ActionMailer::Base
   def contact(contact)
     @contact = contact
-    mail(:to => 'contact@dockyard.com', :from => "#{@contact.name} <#{@contact.email}>", :subject => 'New Contact!')
+    mail(:to => 'sales@dockyard.com', :from => "#{@contact.name} <#{@contact.email}>", :subject => 'New Contact!')
   end
 end
